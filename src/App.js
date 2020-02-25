772bb8c1fd0af8370478184c194ef2b5e792c3eb
curtis-clement <curtis.clement.nl@gmail.com>
2020-02-25 17:19:45 +0100

    Fix Push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Body from './components/Body/Body';
+
+const recent_news = [
+  {
+    title: 'Sample Title',
+    summary: 'Sample Summary',
+    content: 'This will lead you to content',
+    image: 'Sample Image'
+  },
+  {
+    title: 'Another Sample Title',
+    summary: 'Another Sample Summary',
+    content: 'This will lead you to more content',
+    image: 'Another Sample Image'
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body
+         
+      />
     </div>
   );
 }
